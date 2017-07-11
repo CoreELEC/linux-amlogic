c8d6a0cfbf2342aae147a56914fb62b38c2c4066
zdl <823469641@qq.com>
2017-07-11 18:40:23 +0800

    added support for TBS5580 CI box

---
+++ drivers/media/usb/dvb-usb/tbs5580.h
@@ -0,0 +1,8 @@
+#ifndef _TBS5580_H_
+#define _TBS5580_H_
+
+#define DVB_USB_LOG_PREFIX "tbs5580"
+#include "dvb-usb.h"
+
+#define deb_xfer(args...) dprintk(dvb_usb_tbs5580_debug, 0x02, args)
+#endif
