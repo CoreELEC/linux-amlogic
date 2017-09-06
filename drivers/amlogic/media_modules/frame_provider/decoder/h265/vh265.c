a0b848475fcc591f0332cfcd75b38526b1ae3651
Nanxin Qin <nanxin.qin@amlogic.com>
2017-09-07 00:39:08 +0800

    decoder: fixed the issure of mem to avoid leak.
    PD#150221: fixed the issure of mem to avoid leak.
    
    Change-Id: Id1ef51e30cfab718e5e57881af5d84eb2609d1b1
    Signed-off-by: Nanxin Qin <nanxin.qin@amlogic.com>

--- drivers/amlogic/media_modules/frame_provider/decoder/h265/vh265.c
+++ drivers/amlogic/media_modules/frame_provider/decoder/h265/vh265.c
@@ -8735,9 +8735,6 @@ static int vh265_stop(struct hevc_state_s *hevc)
 	uninit_mmu_buffers(hevc);
 	amhevc_disable();
 
-	vfree(hevc->fw);
-	hevc->fw = NULL;
-
 	kfree(gvs);
 	gvs = NULL;
 
@@ -8852,6 +8849,10 @@ static int vmh265_stop(struct hevc_state_s *hevc)
 	cancel_work_sync(&hevc->work);
 	cancel_work_sync(&hevc->notify_work);
 	uninit_mmu_buffers(hevc);
+
+	vfree(hevc->fw);
+	hevc->fw = NULL;
+
 	dump_log(hevc);
 	return 0;
 }
