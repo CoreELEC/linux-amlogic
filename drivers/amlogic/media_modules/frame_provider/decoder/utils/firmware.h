8839e4ae1a97c1355c4cd4b1bbb0fe7d457505e0
Nanxin Qin <nanxin.qin@amlogic.com>
2017-12-06 15:19:29 +0800

    firmware: adds a reload node to avoid the fw load fail [1/1]
    PD#156498: adds a reload node to avoid the fw load fail
    
    usage:
    cat sys/class/firmware_codec/reload
    echo 1 > sys/class/firmware_codec/reload
    echo 2 > sys/class/firmware_codec/reload
    
    set 1 means that the fw is forced to update
    set 2 means that the fw is try to reload
    
    Change-Id: Id453860386cb3eb9dbd21c0ae7238c34655ab1ed
    Signed-off-by: Nanxin Qin <nanxin.qin@amlogic.com>

--- drivers/amlogic/media_modules/frame_provider/decoder/utils/firmware.h
+++ drivers/amlogic/media_modules/frame_provider/decoder/utils/firmware.h
@@ -4,6 +4,9 @@
 #include "../../../common/firmware/firmware_type.h"
 #include <linux/amlogic/media/utils/vformat.h>
 
+#define FW_LOAD_FORCE	(0x1)
+#define FW_LOAD_TRY	(0X2)
+
 struct firmware_s {
 	unsigned int len;
 	char data[0];
@@ -13,5 +16,6 @@ extern int get_decoder_firmware_data(enum vformat_e type,
 	const char *file_name, char *buf, int size);
 extern int get_data_from_name(const char *name, char *buf);
 extern int get_firmware_data(enum firmware_type_e type, char *buf);
+extern int firmware_reload(int mode);
 
 #endif
