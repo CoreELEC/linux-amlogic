a31942ef0858d251b3f89c58f56bed9954429d4a
Chuangcheng Peng <chuangcheng.peng@amlogic.com>
2018-05-22 16:03:45 +0800

    DVB: remove invalid notes
    PD# none
    
    Change-Id: I5b1292cce9d272b61df956e79c7f09adb3986a3d
    Signed-off-by: Chuangcheng Peng <chuangcheng.peng@amlogic.com>

--- drivers/amlogic/media_modules/stream_input/parser/hw_demux/aml_demod_gt.h
+++ drivers/amlogic/media_modules/stream_input/parser/hw_demux/aml_demod_gt.h
@@ -1,20 +1,3 @@
-/*
- * test
- *
- * Copyright (C) 2018 <crope@iki.fi>
- *
- *    This program is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 2 of the License, or
- *    (at your option) any later version.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- */
-
-
 #ifndef __AML_DEMOD_GT_H__
 #define __AML_DEMOD_GT_H__
 
