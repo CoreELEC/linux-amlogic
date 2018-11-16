fb6581fc97c732f1d0a4a8eb4c7b9b7a5070530e
Jian Wang <jian.wang@amlogic.com>
2018-11-16 13:06:27 +0800

    media: license: modify to GPL [1/1]
    PD#SWPL-2111
    
    Problem:
    license source files error
    
    Solution:
    modify to GPL
    
    Verify:
    verify by locally
    
    Change-Id: I77dc054ec4501dd3fb65079a8f437fcc8c3d144f
    Signed-off-by: Jian Wang <jian.wang@amlogic.com>

--- drivers/amlogic/media_modules/stream_input/tv_frontend/dtv_demod/include/amlfrontend.h
+++ drivers/amlogic/media_modules/stream_input/tv_frontend/dtv_demod/include/amlfrontend.h
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) 2014 Amlogic, Inc. All rights reserved.
- *
- * This source code is subject to the terms and conditions defined in the
- * file 'LICENSE' which is part of this source code package.
- *
- * Description:
- */
-
-
 /*****************************************************************
  **
  **  Copyright (C) 2010 Amlogic,Inc.
