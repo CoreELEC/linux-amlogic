5bb92831f8a8176fbfe227b684ce7eba5919439a
Kevin Kim <ckkim@hardkernel.com>
2018-11-29 18:38:05 +0900

    [PATCH] ODROID-COMMON:Add device information to /proc/cpuinfo
    Change-Id: I1148522b11733bfa05a1f74ecb756cf78a0b4fe4

--- arch/arm64/kernel/cpuinfo.c
+++ arch/arm64/kernel/cpuinfo.c
@@ -214,8 +214,9 @@ static int c_show(struct seq_file *m, void *v)
 	}
 	seq_printf(m, "model name\t: Amlogic %s rev %01x\n", meson_name, meson_minor);
 #endif
-
-	seq_printf(m, "Hardware\t: %s\n\n", "Amlogic");
+	system_rev = 0x0400;
+	seq_printf(m, "Hardware\t: %s\n", machine_name);
+	seq_printf(m, "Revision\t: %04x\n\n", system_rev);
 	return 0;
 }
 
