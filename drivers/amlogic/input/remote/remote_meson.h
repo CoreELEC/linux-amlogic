e4b411b2c9543ede3c1ed0f61a76de30f806cf9a
Qianggui Song <qianggui.song@amlogic.com>
2018-12-24 13:35:17 +0800

    ir: add toshiba protocol support [1/1]
    PD#SWPL-3543
    
    Problem:
    current driver does not support toshiba ir remote control protocol
    
    Solution:
    add register setting and decode code for toshiba protocol
    use REMOTE_TYPE_TOSHIBA/REMOTE_TYPE_NEC_TOSHIBA to configure toshiba
    only/toshiba and nec.
    
    Verify:
    tl1_t962x2_x301
    
    Change-Id: Idad70c3879fad6e8267f0c4d80d2447c34114103
    Signed-off-by: Qianggui Song <qianggui.song@amlogic.com>

--- drivers/amlogic/input/remote/remote_meson.h
+++ drivers/amlogic/input/remote/remote_meson.h
@@ -167,23 +167,27 @@ enum {
 };
 
 enum remote_reg {
-	REG_LDR_ACTIVE = 0x00<<2,
-	REG_LDR_IDLE   = 0x01<<2,
-	REG_LDR_REPEAT = 0x02<<2,
-	REG_BIT_0      = 0x03<<2,
-	REG_REG0       = 0x04<<2,
-	REG_FRAME      = 0x05<<2,
-	REG_STATUS     = 0x06<<2,
-	REG_REG1       = 0x07<<2,
-	REG_REG2       = 0x08<<2,
-	REG_DURATN2    = 0x09<<2,
-	REG_DURATN3    = 0x0a<<2,
-	REG_FRAME1     = 0x0b<<2,
-	REG_STATUS1    = 0x0c<<2,
-	REG_STATUS2    = 0x0d<<2,
-	REG_REG3       = 0x0e<<2,
-	REG_FRAME_RSV0 = 0x0f<<2,
-	REG_FRAME_RSV1 = 0x10<<2
+	REG_LDR_ACTIVE = 0x00 << 2,
+	REG_LDR_IDLE   = 0x01 << 2,
+	REG_LDR_REPEAT = 0x02 << 2,
+	REG_BIT_0      = 0x03 << 2,
+	REG_REG0       = 0x04 << 2,
+	REG_FRAME      = 0x05 << 2,
+	REG_STATUS     = 0x06 << 2,
+	REG_REG1       = 0x07 << 2,
+	REG_REG2       = 0x08 << 2,
+	REG_DURATN2    = 0x09 << 2,
+	REG_DURATN3    = 0x0a << 2,
+	REG_FRAME1     = 0x0b << 2,
+	REG_STATUS1    = 0x0c << 2,
+	REG_STATUS2    = 0x0d << 2,
+	REG_REG3       = 0x0e << 2,
+	REG_FRAME_RSV0 = 0x0f << 2,
+	REG_FRAME_RSV1 = 0x10 << 2,
+	REG_FILTE      = 0x11 << 2,
+	REG_IRQ_CTL    = 0x12 << 2,
+	REG_WIDTH_NEW  = 0x14 << 2,
+	REG_REPEAT_DET = 0x15 << 2
 };
 
 int ir_register_default_config(struct remote_chip *chip, int type);
