e7af75d340a5295828da18e6acb07b4f8c173395
Yong Qin <yong.qin@amlogic.com>
2018-12-25 11:03:01 +0800

    di: disable afbc on tl1  [1/1]
    PD#SWPL-3413
    
    Problem:
    when playing SD/HD/VP9/H265 video, there
    are 3 veritcal lines on the picture.
    
    Solution:
    disable afbc
    
    Verify:
    verified on tl1
    
    Change-Id: I839d8f26aefdf2c71272d8d3e7bf93e4c3294491
    Signed-off-by: Yong Qin <yong.qin@amlogic.com>

--- drivers/amlogic/media/deinterlace/deinterlace_hw.c
+++ drivers/amlogic/media/deinterlace/deinterlace_hw.c
@@ -1004,7 +1004,7 @@ bool afbc_is_supported(void)
 	/*currently support txlx and g12a*/
 	if (is_meson_txlx_cpu()
 		|| is_meson_g12a_cpu()
-		|| is_meson_tl1_cpu())
+		/*|| is_meson_tl1_cpu()*/)
 		ret = true;
 	return ret;
 
