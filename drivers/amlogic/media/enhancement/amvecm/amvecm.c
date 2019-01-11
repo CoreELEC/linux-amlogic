1d27f9066c8771901e0aafcd76c5579d795d3426
Bencheng Jing <bencheng.jing@amlogic.com>
2019-01-11 14:32:30 +0800

    amvecm: modify gamma_init to avoid lcd black screen after resume [1/1]
    PD#SWPL-4101
    
    Problem:
    lcd black screen after resume
    
    Solution:
    init gamma data
    
    Verify:
    x301
    
    Change-Id: Ibc04a96f00948bc4780c3eb1452bca1c6ae92961
    Signed-off-by: Bencheng Jing <bencheng.jing@amlogic.com>

--- drivers/amlogic/media/enhancement/amvecm/amvecm.c
+++ drivers/amlogic/media/enhancement/amvecm/amvecm.c
@@ -5798,16 +5798,16 @@ void amvecm_gamma_init(bool en)
 	unsigned int i;
 	unsigned short data[256];
 
+	for (i = 0; i < 256; i++) {
+		data[i] = i << 2;
+		video_gamma_table_r.data[i] = data[i];
+		video_gamma_table_g.data[i] = data[i];
+		video_gamma_table_b.data[i] = data[i];
+	}
+
 	if (en) {
 		WRITE_VPP_REG_BITS(L_GAMMA_CNTL_PORT,
 				0, GAMMA_EN, 1);
-
-		for (i = 0; i < 256; i++) {
-			data[i] = i << 2;
-			video_gamma_table_r.data[i] = data[i];
-			video_gamma_table_g.data[i] = data[i];
-			video_gamma_table_b.data[i] = data[i];
-		}
 		amve_write_gamma_table(
 					data,
 					H_SEL_R);
