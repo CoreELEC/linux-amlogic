a922e5ebc5e705b6f2363ca7e87025d0a332220e
Joy Cho <joy.cho@hardkernel.com>
2019-02-19 10:30:18 +0900

    gpio_keypad: add wake up gpio set by config.ini
    ODROID-N2: keyboard: add gpio power key that is set from boot.ini
      by: Joy Cho <joy.cho@hardkernel.com>	2019-02-19 10:30:18 +0900
    
    gpio_keypad: add support of all gpios for wake up
      by: Portisch <hugo.portisch@yahoo.de>	2019-10-09 15:15:07 +0200

--- drivers/amlogic/input/keyboard/gpio_keypad.c
+++ drivers/amlogic/input/keyboard/gpio_keypad.c
@@ -19,6 +19,7 @@
 #include <linux/input.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
+#include <linux/gpio.h>
 #include <linux/gpio/consumer.h>
 #include <linux/interrupt.h>
 #include <linux/slab.h>
@@ -32,6 +33,9 @@
 #define DEFAULT_SCAN_PERION	20
 #define	DEFAULT_POLL_MODE	0
 #define KEY_JITTER_COUNT	1
+
+#define AO_DEBUG_REG0		((0x28 << 2))
+static unsigned long gpiopower;
 
 struct pin_desc {
 	int current_status;
@@ -54,6 +58,29 @@ struct gpio_keypad {
 	struct timer_list polling_timer;
 	struct input_dev *input_dev;
 };
+
+enum gpio_bank {
+	PERIPHS,
+	AOBUS,
+	UNKNOWN = 0xFF
+};
+
+static int __init gpiopower_setup(char *str)
+{
+	int ret;
+
+	if (str == NULL) {
+		gpiopower = 0;
+		return -EINVAL;
+	}
+
+	ret = kstrtoul(str, 0, &gpiopower);
+
+	pr_info("%s gpiopower : %ld\n", __func__, gpiopower);
+
+	return 0;
+}
+__setup("gpiopower=", gpiopower_setup);
 
 static irqreturn_t gpio_irq_handler(int irq, void *data)
 {
@@ -153,10 +180,15 @@ static void polling_timer_handler(unsigned long data)
 static int meson_gpio_kp_probe(struct platform_device *pdev)
 {
 	struct gpio_desc *desc;
+	struct gpio_chip *chip;
+	enum gpio_bank bank = UNKNOWN;
 	int ret, i;
 	struct input_dev *input_dev;
 	struct gpio_keypad *keypad;
 	bool pwrkey_update_flg = false;
+	struct resource *res;
+	int val, err;
+	void __iomem *ao_reg = NULL;
 
 	if (!(pdev->dev.of_node)) {
 		dev_err(&pdev->dev,
@@ -188,11 +220,61 @@ static int meson_gpio_kp_probe(struct platform_device *pdev)
 		(keypad->key_size)*sizeof(*(keypad->key)), GFP_KERNEL);
 	if (!(keypad->key))
 		return -EINVAL;
+	if (gpiopower) {
+		res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+		if (res) {
+			ao_reg = ioremap(res->start, res->end - res->start);
+		} else {
+			dev_info(&pdev->dev, "Failed to get AO reg\n");
+		}
+	}
 	for (i = 0; i < keypad->key_size; i++) {
-		//get all gpio desc.
-		desc = devm_gpiod_get_index(&pdev->dev, "key", i, GPIOD_IN);
-		if (IS_ERR_OR_NULL(desc))
-			return -EINVAL;
+		if (!gpiopower) {
+			//get all gpio desc.
+			desc = devm_gpiod_get_index(&pdev->dev, "key", i, GPIOD_IN);
+			if (IS_ERR_OR_NULL(desc))
+				return -EINVAL;
+		} else {
+			if (gpio_is_valid(gpiopower)) {
+				err = devm_gpio_request_one(&pdev->dev,
+					gpiopower, GPIOF_IN, "gpio_keys");
+				if (err < 0) {
+					dev_err(&pdev->dev, "Failed to request GPIO %ld, error %d\n",
+						gpiopower, err);
+					return -EINVAL;
+				}
+				desc = gpio_to_desc(gpiopower);
+				if (IS_ERR_OR_NULL(desc)) {
+					dev_err(&pdev->dev, "Failed to get desc\n");
+					return -EINVAL;
+				}
+
+				chip = gpiod_to_chip(desc);
+
+				if (IS_ERR_OR_NULL(chip)) {
+					dev_err(&pdev->dev, "Failed to get chip from desc\n");
+					return -EINVAL;
+				}
+
+				if (!strcmp(chip->label, "periphs-banks"))
+					bank = PERIPHS;
+				else if (!strcmp(chip->label, "aobus-banks"))
+					bank = AOBUS;
+
+				if (!IS_ERR_OR_NULL(ao_reg) && bank != UNKNOWN) {
+					val = readl((ao_reg + AO_DEBUG_REG0)) & 0xFFFF;
+					val |= ((gpiopower - chip->base) & 0xFFF) << 16;
+					val |= bank << 28;
+					writel(val, (ao_reg + AO_DEBUG_REG0));
+				}
+				else
+					dev_err(&pdev->dev, "invalid chip->label: %s, ao_reg: %p\n", chip->label, ao_reg);
+			} else {
+				dev_err(&pdev->dev, "invalid gpio %ld\n",
+						gpiopower);
+				return -EINVAL;
+			}
+		}
 		keypad->key[i].desc = desc;
 		//The gpio default is high level.
 		keypad->key[i].current_status = 1;
