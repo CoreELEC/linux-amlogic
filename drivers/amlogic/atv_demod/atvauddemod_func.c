6cd6de0ef38d38874d9c3f22541d5435b8c99109
nengwen.chen <nengwen.chen@amlogic.com>
2019-02-28 14:43:14 +0800

    atv_demod: Fix non standard for analog signal [1/1]
    PD#TV-1549, PD#TV-2829, PD#TV-2830
    
    Problem:
    Fix non standard for analog signal.
    
    Solution:
    1.Add non standard config 3 for analog signal.
    2.Fix non standard default value.
    3.Fix audio control register an bits.
    4.After correcting the frequency offset success,
     reset tuner to prevent audio from identifying errors.
    
    Verify:
    verified by x301
    
    Change-Id: I65335dbe9ce9dc66d4ccaae3c1ff591660790616
    Signed-off-by: nengwen.chen <nengwen.chen@amlogic.com>

--- drivers/amlogic/atv_demod/atvauddemod_func.c
+++ drivers/amlogic/atv_demod/atvauddemod_func.c
@@ -1521,8 +1521,15 @@ void set_output_left_right_exchange(unsigned int ch)
 
 	atvaudio_ctrl_read(&read);
 
-	if ((read & (1 << 2)) != ((ch & 0x01) << 2))
-		atvaudio_ctrl_write((read & ~(1 << 2)) | ((ch & 0x01) << 2));
+	if (is_meson_tl1_cpu()) { /* bit[19] */
+		if ((read & (1 << 19)) != ((ch & 0x01) << 19))
+			atvaudio_ctrl_write((read & ~(1 << 19)) |
+					((ch & 0x01) << 19));
+	} else { /* bit[2] */
+		if ((read & (1 << 2)) != ((ch & 0x01) << 2))
+			atvaudio_ctrl_write((read & ~(1 << 2)) |
+					((ch & 0x01) << 2));
+	}
 }
 
 #endif /* __ATVAUDDEMOD_FUN_H */
