c33092b845d3b5540d83a63cf6f00d25f6b43621
Arthur Liberman <arthur_liberman@hotmail.com>
2019-03-02 23:20:14 +0200

    net: phy: 8211f disable eee led indication

--- drivers/net/phy/realtek.c
+++ drivers/net/phy/realtek.c
@@ -26,6 +26,7 @@
 #include <linux/amlogic/pm.h>
 #endif
 #define RTL821x_LCR		0x10
+#define RTL821x_EEELCR		0x11
 #define RTL821x_PHYSR		0x11
 #define RTL821x_PHYSR_DUPLEX	0x2000
 #define RTL821x_PHYSR_SPEED	0xc000
@@ -152,6 +153,7 @@ static int rtl8211f_config_init(struct phy_device *phydev)
 #endif
 	phy_write(phydev, RTL8211F_PAGE_SELECT, 0xd04); /*set page 0xd04*/
 	phy_write(phydev, RTL821x_LCR, 0XC171); /*led configuration*/
+	phy_write(phydev, RTL821x_EEELCR, 0); /*disable eee led indication*/
 
 	/* restore to default page 0 */
 	phy_write(phydev, RTL8211F_PAGE_SELECT, 0x0);
