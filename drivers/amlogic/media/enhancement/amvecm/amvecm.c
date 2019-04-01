dcb1587f3e859a1c41fc63b5ad74bac29de63e1a
Yong Qin <yong.qin@amlogic.com>
2019-04-01 15:45:36 +0800

    vlock: bring up for tm2 [1/1]
    PD#SWPL-6576
    
    Problem:
    bringup for tm2
    
    Solution:
    add some chip id for match tm2
    
    Verify:
    tm2
    
    Change-Id: I857ec687b7888782efb8875f8edee538dd615947
    Signed-off-by: Yong Qin <yong.qin@amlogic.com>

--- drivers/amlogic/media/enhancement/amvecm/amvecm.c
+++ drivers/amlogic/media/enhancement/amvecm/amvecm.c
@@ -6532,6 +6532,14 @@ static const struct vecm_match_data_s vecm_dt_tl1 = {
 	.vlk_phlock_en = false,
 };
 
+static const struct vecm_match_data_s vecm_dt_tm2 = {
+	.vlk_support = true,
+	.vlk_new_fsm = 1,
+	.vlk_hwver = vlock_hw_ver2,
+	.vlk_phlock_en = false,
+};
+
+
 static const struct of_device_id aml_vecm_dt_match[] = {
 	{
 		.compatible = "amlogic, vecm",
@@ -6540,6 +6548,10 @@ static const struct of_device_id aml_vecm_dt_match[] = {
 	{
 		.compatible = "amlogic, vecm-tl1",
 		.data = &vecm_dt_tl1,
+	},
+	{
+		.compatible = "amlogic, vecm-tm2",
+		.data = &vecm_dt_tm2,
 	},
 	{},
 };
