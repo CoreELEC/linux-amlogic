49de4d96d8ea6fc62e41db96dfa97321b8dbc0f3
cdu13a <cdu13a@gmail.com>
2019-04-10 23:58:08 -0400

    hdmitx: add "now" to attr to trigger set_disp_mode_auto()
    Useful for testing. Adding "now" to attr is a one shot way to force
    changes to current hdmi settings to take effect imediatly instead of
    waiting for a mode change.

--- drivers/amlogic/media/vout/hdmitx/hdmi_tx_20/hdmi_tx_main.c
+++ drivers/amlogic/media/vout/hdmitx/hdmi_tx_20/hdmi_tx_main.c
@@ -590,7 +590,7 @@ static int set_disp_mode_auto(void)
 	else {
 	/* nothing */
 	}
-	if ((vic_ready != HDMI_Unknown) && (vic_ready == vic)) {
+	if ((vic_ready != HDMI_Unknown) && (vic_ready == vic) && (strstr(hdmitx_device.fmt_attr,"now") == NULL)) {
 		pr_info(SYS "[%s] ALREADY init VIC = %d\n",
 			__func__, vic);
 		if (hdev->rxcap.ieeeoui == 0) {
@@ -617,6 +617,10 @@ static int set_disp_mode_auto(void)
 
 	hdmitx_pre_display_init();
 
+
+	if (strstr(hdmitx_device.fmt_attr,"now") != NULL){
+		memcpy(strstr(hdmitx_device.fmt_attr,"now"), " ", 3);
+	}
 	hdev->cur_VIC = HDMI_Unknown;
 /* if vic is HDMI_Unknown, hdmitx_set_display will disable HDMI */
 	ret = hdmitx_set_display(hdev, vic);
@@ -696,6 +700,10 @@ ssize_t store_attr(struct device *dev,
 		hdmitx_device.para->cs = COLORSPACE_YUV420;
 	else
 		hdmitx_device.para->cs = COLORSPACE_YUV444;
+
+	if (strstr(hdmitx_device.fmt_attr,"now")){
+		set_disp_mode_auto();
+	}
 	return count;
 }
 /*aud_mode attr*/
