dd40cac611121797ca8d8f40ac47dd370b5d0d67
Yue Wang <yue.wang@amlogic.com>
2019-04-24 16:39:21 +0800

    usb: usb warning [1/1]
    PD#SWPL-7031
    
    Problem:
    usb warning.
    
    Solution:
    delete usb warning.
    
    Verify:
    TM2
    
    Change-Id: I70cf1f71bfaed0e7b9b53118259e86806933281f
    Signed-off-by: Yue Wang <yue.wang@amlogic.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -330,12 +330,16 @@ static void dwc3_frame_length_adjustment(struct dwc3 *dwc)
 
 	reg = dwc3_readl(dwc->regs, DWC3_GFLADJ);
 	dft = reg & DWC3_GFLADJ_30MHZ_MASK;
+#ifndef CONFIG_AMLOGIC_USB
 	if (!dev_WARN_ONCE(dwc->dev, dft == dwc->fladj,
 	    "request value same as default, ignoring\n")) {
+#endif
 		reg &= ~DWC3_GFLADJ_30MHZ_MASK;
 		reg |= DWC3_GFLADJ_30MHZ_SDBND_SEL | dwc->fladj;
 		dwc3_writel(dwc->regs, DWC3_GFLADJ, reg);
+#ifndef CONFIG_AMLOGIC_USB
 	}
+#endif
 }
 
 /**
