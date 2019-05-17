3604f6922944f2d160379c7967fd10916f80895e
Nick <nick@khadas.com>
2019-05-17 16:20:24 +0800

    vout: fix hdmi display hotplug issue

--- drivers/amlogic/media/vout/hdmitx/hdmi_tx_20/hdmi_tx_main.c
+++ drivers/amlogic/media/vout/hdmitx/hdmi_tx_20/hdmi_tx_main.c
@@ -5855,7 +5855,7 @@ static int hdmitx_set_current_vmode(enum vmode_e mode, void *data)
 		recalc_vinfo_sync_duration(vinfo,
 					   hdmitx_device.frac_rate_policy);
 
-	if (!(mode & VMODE_INIT_BIT_MASK)) {
+	if (!(mode & VMODE_INIT_BIT_MASK) && get_hpd_state()) {
 		set_disp_mode_auto();
 	} else {
 		pr_info("alread display in uboot\n");
