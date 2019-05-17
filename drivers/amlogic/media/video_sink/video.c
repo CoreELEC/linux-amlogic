8fb20103e9abcbfe6c91f7593397a7ab8f77248f
Nick <nick@khadas.com>
2019-05-17 16:02:49 +0800

    media: improve the video playback

--- drivers/amlogic/media/video_sink/video.c
+++ drivers/amlogic/media/video_sink/video.c
@@ -6823,6 +6823,10 @@ static irqreturn_t vsync_isr_in(int irq, void *dev_id)
 		unsigned long delta1 = 0;
 
 		diff = system_time - omx_pts;
+
+		video_notify_flag |= VIDEO_NOTIFY_TRICK_WAIT;
+		atomic_set(&trickmode_framedone, 1);
+
 		if (time_setomxpts > 0
 			&& time_setomxpts_last > 0) {
 			/* time_setomxpts record hwc setomxpts time, */
