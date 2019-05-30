221652b16b7ea846e6f89f7f4329bb9780064b47
Zongdong Jiao <zongdong.jiao@amlogic.com>
2019-05-30 20:08:41 +0800

    hdmitx: edid: add suitable case for DolbyVision [1/1]
    PD#SWPL-7888
    
    Problem:
    DV tests add more EDID cases
    v2_yuv444_2_yuv422_0_int_3_12Byte.xml
    v2_yuv444_0_yuv422_0_int_2_13Byte.xml
    v2_yuv444_0_yuv422_0_int_2_16Byte.xml
    
    Solution:
    add suitable case for DolbyVision
    
    Verify:
    G12/U212
    
    Change-Id: I323d49ad34b12b6a12ba93f0349f0398f2516bcb
    Signed-off-by: Zongdong Jiao <zongdong.jiao@amlogic.com>

--- drivers/amlogic/media/vout/hdmitx/hdmi_tx_20/hdmi_tx_main.c
+++ drivers/amlogic/media/vout/hdmitx/hdmi_tx_20/hdmi_tx_main.c
@@ -2462,6 +2462,7 @@ static ssize_t show_dc_cap(struct device *dev,
 	enum hdmi_vic vic = HDMI_Unknown;
 	int pos = 0;
 	struct rx_cap *pRXCap = &(hdmitx_device.RXCap);
+	const struct dv_info *dv = &(hdmitx_device.RXCap.dv_info);
 
 #if 0
 	if (pRXCap->dc_48bit_420)
@@ -2496,9 +2497,9 @@ static ssize_t show_dc_cap(struct device *dev,
 	}
 next444:
 	if (pRXCap->dc_y444) {
-		if (pRXCap->dc_36bit)
+		if ((pRXCap->dc_36bit) || (dv->sup_10b_12b_444 == 0x2))
 			pos += snprintf(buf + pos, PAGE_SIZE, "444,12bit\n");
-		if (pRXCap->dc_30bit) {
+		if ((pRXCap->dc_30bit) || (dv->sup_10b_12b_444 == 0x1)) {
 			pos += snprintf(buf + pos, PAGE_SIZE, "444,10bit\n");
 			pos += snprintf(buf + pos, PAGE_SIZE, "444,8bit\n");
 		}
@@ -2506,7 +2507,7 @@ next444:
 		if (pRXCap->dc_48bit)
 			pos += snprintf(buf + pos, PAGE_SIZE, "444,16bit\n");
 #endif
-		if (pRXCap->dc_36bit)
+		if ((pRXCap->dc_36bit) || (dv->sup_yuv422_12bit))
 			pos += snprintf(buf + pos, PAGE_SIZE, "422,12bit\n");
 		if (pRXCap->dc_30bit) {
 			pos += snprintf(buf + pos, PAGE_SIZE, "422,10bit\n");
@@ -2524,9 +2525,9 @@ nextrgb:
 	if (pRXCap->dc_48bit)
 		pos += snprintf(buf + pos, PAGE_SIZE, "rgb,16bit\n");
 #endif
-	if (pRXCap->dc_36bit)
+	if ((pRXCap->dc_36bit) || (dv->sup_10b_12b_444 == 0x2))
 		pos += snprintf(buf + pos, PAGE_SIZE, "rgb,12bit\n");
-	if (pRXCap->dc_30bit)
+	if ((pRXCap->dc_30bit) || (dv->sup_10b_12b_444 == 0x1))
 		pos += snprintf(buf + pos, PAGE_SIZE, "rgb,10bit\n");
 	pos += snprintf(buf + pos, PAGE_SIZE, "rgb,8bit\n");
 	return pos;
