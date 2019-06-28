44318e5c204b261e89e70b9599695f99f96193ab
yao liu <yao.liu@amlogic.com>
2019-06-28 21:16:21 +0800

    dolby: demo-debug.apk test fail [1/1]
    PD#SWPL-10167
    
    Problem:
    Apk drop several frames at beginning, but
    dolby frames are not consumed, decoder has
    no idle buffers.
    
    Solution:
    Ignore drop for dual layer dolby video
    
    Verify:
    verified on u212
    
    Change-Id: I732d82e71c190c62a14c05accea44b4c55d04239
    Signed-off-by: yao liu <yao.liu@amlogic.com>

--- include/linux/amlogic/media/amdolbyvision/dolby_vision.h
+++ include/linux/amlogic/media/amdolbyvision/dolby_vision.h
@@ -76,4 +76,5 @@ extern bool is_dovi_frame(struct vframe_s *vf);
 extern void update_graphic_width_height(unsigned int width,
 	unsigned int height);
 extern bool is_dolby_vision_el_disable(void);
+extern bool is_dovi_dual_layer_frame(struct vframe_s *vf);
 #endif
