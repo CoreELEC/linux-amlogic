97b3fbc9ac03522843ec32d80ea0380f8d23b472
Portisch <hugo.portisch@yahoo.de>
2019-08-07 10:40:12 +0200

    stmmac_platform: u-boot mac is first priority

--- drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -199,6 +199,41 @@ static int stmmac_dt_phy(struct plat_stmmacenet_data *plat,
 	return 0;
 }
 
+#ifdef CONFIG_DWMAC_MESON
+u8 DEFMAC[] = {0, 0, 0, 0, 0, 0};
+bool g_mac_addr_setup = false;
+static unsigned char chartonum(char c)
+{
+	if (c >= '0' && c <= '9')
+		return c - '0';
+	if (c >= 'A' && c <= 'F')
+		return (c - 'A') + 10;
+	if (c >= 'a' && c <= 'f')
+		return (c - 'a') + 10;
+	return 0;
+
+}
+
+static int __init mac_addr_set(char *line)
+{
+	unsigned char mac[6];
+	int i = 0;
+	for (i = 0; i < 6 && line[0] != '\0' && line[1] != '\0'; i++) {
+		mac[i] = chartonum(line[0]) << 4 | chartonum(line[1]);
+		line += 3;
+	}
+	memcpy(DEFMAC, mac, 6);
+	pr_debug("uboot setup mac-addr: %x:%x:%x:%x:%x:%x\n",
+		DEFMAC[0], DEFMAC[1], DEFMAC[2], DEFMAC[3], DEFMAC[4],
+		DEFMAC[5]);
+	g_mac_addr_setup = true;
+
+	return 1;
+}
+
+__setup("mac=", mac_addr_set);
+#endif
+
 /**
  * stmmac_probe_config_dt - parse device-tree driver parameters
  * @pdev: platform_device structure
@@ -218,7 +253,15 @@ stmmac_probe_config_dt(struct platform_device *pdev, const char **mac)
 	if (!plat)
 		return ERR_PTR(-ENOMEM);
 
+#ifdef CONFIG_DWMAC_MESON
+	if (g_mac_addr_setup)	/*so uboot mac= is first priority.*/
+		*mac = DEFMAC;
+	else
+		*mac = of_get_mac_address(np);
+#else
 	*mac = of_get_mac_address(np);
+#endif
+
 	plat->interface = of_get_phy_mode(np);
 
 	/* Get max speed of operation from device tree */
