b27dce5e791e482e929d9f2eccaf75908abff4c8
Portisch <hugo.portisch@yahoo.de>
2019-08-19 13:19:17 +0200

    bl301_manager: implement bl301_manager platform driver This small driver handle data from config.ini to the bootloader blob bl301.

---
+++ drivers/bootloader/bl301_manager.h
@@ -0,0 +1,27 @@
+/*
+ * Driver for manage data between config.ini and bootloader blob bl301
+ *
+ * Copyright (C) 2019 Team CoreELEC
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
+#ifndef _BL301_MANAGER_H_
+#define _BL301_MANAGER_H_
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/amlogic/scpi_protocol.h>
+
+#define DRIVER_NAME	"bl301_manager"
+
+#endif /* _BL301_MANAGER_H_ */
