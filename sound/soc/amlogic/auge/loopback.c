bf5471216b68e5a4a6d8a25fb86e5cbcf62455c9
Shuai Li <shuai.li@amlogic.com>
2019-08-30 18:38:21 +0800

    audio: make loopback compitable with chips [1/1]
    PD#TV-8363
    
    Problem:
    Loopback doesn't work on TM2.
    TL1 doesn't support loopback in OE pins.
    
    Solution:
    Make it compitable with chips.
    Add tdm_lb format and chmasks.
    
    Verify:
    TM2 AB311 and TL1 X301.
    
    Change-Id: If878fe75fc87857eae7a44439a451c5b2f494c67
    Signed-off-by: Shuai Li <shuai.li@amlogic.com>

--- sound/soc/amlogic/auge/loopback.c
+++ sound/soc/amlogic/auge/loopback.c
@@ -79,7 +79,8 @@ struct loopback {
 	unsigned int datalb_chnum;
 	unsigned int datalb_chmask;
 	unsigned int datalb_lane_mask; /* related with data lane */
-
+	unsigned int lb_format;
+	unsigned int lb_lane_chmask;
 	unsigned int sysclk_freq;
 
 	struct toddr *tddr;
@@ -434,7 +435,7 @@ static int loopback_dai_startup(
 
 	/* datalb */
 	switch (p_loopback->datalb_src) {
-	case TDMINLB_TDMOUTA ... TDMINLB_PAD_TDMINC:
+	case TDMINLB_TDMOUTA ... TDMINLB_PAD_TDMINC_D:
 		/*tdminlb_startup(p_loopback);*/
 		break;
 	case SPDIFINLB_SPDIFOUTA ... SPDIFINLB_SPDIFOUTB:
@@ -476,7 +477,7 @@ static void loopback_dai_shutdown(
 
 	/* datalb */
 	switch (p_loopback->datalb_src) {
-	case TDMINLB_TDMOUTA ... TDMINLB_PAD_TDMINC:
+	case TDMINLB_TDMOUTA ... TDMINLB_PAD_TDMINC_D:
 		/*tdminlb_shutdown(p_loopback);*/
 		break;
 	case SPDIFINLB_SPDIFOUTA ... SPDIFINLB_SPDIFOUTB:
@@ -558,12 +559,7 @@ static int loopback_set_ctrl(struct loopback *p_loopback, int bitwidth)
 	}
 
 	switch (p_loopback->datalb_src) {
-	case TDMINLB_TDMOUTA:
-	case TDMINLB_TDMOUTB:
-	case TDMINLB_TDMOUTC:
-	case TDMINLB_PAD_TDMINA:
-	case TDMINLB_PAD_TDMINB:
-	case TDMINLB_PAD_TDMINC:
+	case TDMINLB_TDMOUTA ... TDMINLB_PAD_TDMINC_D:
 		if (bitwidth == 24) {
 			datalb_toddr_type = 4;
 			datalb_msb = 32 - 1;
@@ -607,9 +603,10 @@ static int loopback_set_ctrl(struct loopback *p_loopback, int bitwidth)
 	lb_set_datain_cfg(p_loopback->id, &datain_cfg);
 	lb_set_datalb_cfg(p_loopback->id, &datalb_cfg);
 
-	tdminlb_set_format(1); /* tdmin_lb i2s mode */
+	tdminlb_set_format(p_loopback->lb_format == SND_SOC_DAIFMT_I2S);
 	tdminlb_set_lanemask_and_chswap(0x76543210,
-		p_loopback->datalb_lane_mask);
+		p_loopback->datalb_lane_mask,
+		p_loopback->lb_lane_chmask);
 	tdminlb_set_ctrl(p_loopback->datalb_src);
 
 	return 0;
@@ -721,6 +718,10 @@ static int loopback_dai_prepare(
 		case TDMINLB_PAD_TDMINA:
 		case TDMINLB_PAD_TDMINB:
 		case TDMINLB_PAD_TDMINC:
+			break;
+		case TDMINLB_PAD_TDMINA_D:
+		case TDMINLB_PAD_TDMINB_D:
+		case TDMINLB_PAD_TDMINC_D:
 			break;
 		case SPDIFINLB_SPDIFOUTA:
 		case SPDIFINLB_SPDIFOUTB:
@@ -891,7 +892,7 @@ static int loopback_dai_hw_params(
 
 	/* datalb */
 	switch (p_loopback->datalb_src) {
-	case TDMINLB_TDMOUTA ... TDMINLB_PAD_TDMINC:
+	case TDMINLB_TDMOUTA ... TDMINLB_PAD_TDMINC_D:
 		/*datalb_tdminlb_set_clk(p_loopback);*/
 		break;
 	case SPDIFINLB_SPDIFOUTA ... SPDIFINLB_SPDIFOUTB:
@@ -1038,6 +1039,9 @@ static const char *const datalb_tdminlb_texts[] = {
 	"TDMIN_A",
 	"TDMIN_B",
 	"TDMIN_C",
+	"TDMIN_A_D",
+	"TDMIN_B_D",
+	"TDMIN_C_D",
 };
 
 static const struct soc_enum datalb_tdminlb_enum =
@@ -1277,6 +1281,39 @@ err:
 	return -EINVAL;
 }
 
+static unsigned int loopback_parse_format(struct device_node *node)
+{
+	unsigned int format = 0;
+	int ret = 0;
+	const char *str;
+	struct {
+		char *name;
+		unsigned int val;
+	} fmt_table[] = {
+		{"i2s", SND_SOC_DAIFMT_I2S},
+		{"dsp_a", SND_SOC_DAIFMT_DSP_A},
+		{"dsp_b", SND_SOC_DAIFMT_DSP_B}
+	};
+
+	ret = of_property_read_string(node, "datalb-format", &str);
+	if (ret == 0) {
+		int i;
+
+		for (i = 0; i < ARRAY_SIZE(fmt_table); i++) {
+			if (strcmp(str, fmt_table[i].name) == 0) {
+				format |= fmt_table[i].val;
+				break;
+			}
+		}
+	}
+
+	/* default format is I2S */
+	if (format == 0)
+		format = SND_SOC_DAIFMT_I2S;
+
+	return format;
+}
+
 static int loopback_parse_of(
 	struct device_node *node,
 	struct loopback *p_loopback)
@@ -1350,6 +1387,15 @@ static int loopback_parse_of(
 		goto fail;
 	}
 
+	p_loopback->lb_format = loopback_parse_format(node);
+	snd_soc_of_get_slot_mask
+		(node,
+		"datalb-channels-mask",
+		&p_loopback->lb_lane_chmask);
+	if (p_loopback->lb_lane_chmask == 0) {
+		/* default format is I2S and mask two channels */
+		p_loopback->lb_lane_chmask = 0x3;
+	}
 	pr_info("\tdatain_src:%d, datain_chnum:%d, datain_chumask:%x\n",
 		p_loopback->datain_src,
 		p_loopback->datain_chnum,
@@ -1361,6 +1407,8 @@ static int loopback_parse_of(
 	pr_info("\tdatain_lane_mask:0x%x, datalb_lane_mask:0x%x\n",
 		p_loopback->datain_lane_mask,
 		p_loopback->datalb_lane_mask);
+	pr_info("datalb_format: %d, chmask for lanes: %#x\n",
+		p_loopback->lb_format, p_loopback->lb_lane_chmask);
 
 	ret = datain_parse_of(node, p_loopback);
 	if (ret) {
