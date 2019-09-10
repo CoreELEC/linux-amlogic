c7dbd68dfafeaf65a99cfe4d93aa74fae772d2a0
he.he <he.he@amlogic.com>
2019-09-09 22:48:17 -0400

    usb: TOSHIBA Disk Can't Be Recognized on the Port of USB 2.0 [1/1]
    PD#SWPL-14156
    
    Problem:
    TOSHIBA Disk can't be recognized on the Port of USB 2.0 and since then
    any disk can't be recognized.This is because
    that the CCS flag of the PORTSC is still set and if write 0
    to 0x38 of usb phy register the CCS will change to
    0 and so that other disks can be recognized.
    
    Solution:
    when the enumeration fails, call set_usb_phy_host_tuning.
    
    Verify:
    test pass on u212
    
    Change-Id: I507f269afc825de75c7dcce5f79c9c1dd7793d84
    Signed-off-by: he.he <he.he@amlogic.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4982,6 +4982,9 @@ loop:
 
 done:
 	hub_port_disable(hub, port1, 1);
+#ifdef CONFIG_AMLOGIC_USB2PHY
+	set_usb_phy_host_tuning(port1 - 1, 1);
+#endif
 	if (hcd->driver->relinquish_port && !hub->hdev->parent) {
 		if (status != -ENOTCONN && status != -ENODEV)
 			hcd->driver->relinquish_port(hcd, port1);
