24e44b40e8908ad6fa572539a362a19690179f57
Yong Qin <yong.qin@amlogic.com>
2019-12-09 17:34:17 +0800

    cec: sm1 send bus level check fail [1/1]
    PD#SWPL-17583
    
    Problem:
    cec send msg, cec bus level check fail.
    the register define is not right.
    
    Solution:
    cec bus input register is PREG_PAD_GPIO3_I.
    
    Verify:
    SM1
    
    Change-Id: I2eb49cab79a0141141ae3fafb704239c2c779485
    Signed-off-by: Yong Qin <yong.qin@amlogic.com>
    (cherry picked from commit 4364fdedbd5276f04f2f46fc2e81988d77370de5)

--- drivers/amlogic/cec/hdmi_ao_cec.h
+++ drivers/amlogic/cec/hdmi_ao_cec.h
@@ -18,7 +18,7 @@
 #ifndef __AO_CEC_H__
 #define __AO_CEC_H__
 
-#define CEC_DRIVER_VERSION     "2019/12/01:finetune ARB time, cause cts 9.6.1\n"
+#define CEC_DRIVER_VERSION     "2019/12/09:sm1 bus check reg define err\n"
 
 #define CEC_FRAME_DELAY		msecs_to_jiffies(30)
 #define CEC_CHK_BUS_CNT		20
