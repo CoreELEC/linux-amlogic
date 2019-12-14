c29e6be879a7ef591d465433e99b53d87d82c0e4
Portisch <hugo.portisch@yahoo.de>
2019-12-14 23:29:28 +0000

    video_sink: add amvideocap module
    If the frame rate is higher than 30 fps each second frame is captured.
    
    stretchblt: fix missing block assignment in _stretchblt_noalpha

--- include/linux/amlogic/media/vfm/vframe.h
+++ include/linux/amlogic/media/vfm/vframe.h
@@ -522,8 +522,6 @@ struct vframe_s {
 	u64 ready_jiffies64;	/* ready from decode on  jiffies_64 */
 	long long ready_clock[5];/*ns*/
 	long long ready_clock_hist[2];/*ns*/
-	atomic_t use_cnt;
-	atomic_t use_cnt_pip;
 	u32 frame_dirty;
 	/*
 	 *prog_proc_config:
