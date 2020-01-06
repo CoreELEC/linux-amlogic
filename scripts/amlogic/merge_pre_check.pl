5143761dea7cd3b0d46c36b0978473b96c013993
Jianxiong Pan <jianxiong.pan@amlogic.com>
2020-01-06 15:32:51 +0800

    script: add check whether the PD is "SWPL". [1/1]
    PD#SWPL-19506
    
    Problem:
    add check whether the PD is "SWPL".
    
    Solution:
    modify the merge_pre_check.pl check script.
    
    Verify:
    local test.
    
    Change-Id: I24b69fadb1fd2fbb914ced7af8c34f200029e33d
    Signed-off-by: Jianxiong Pan <jianxiong.pan@amlogic.com>

--- scripts/amlogic/merge_pre_check.pl
+++ scripts/amlogic/merge_pre_check.pl
@@ -139,7 +139,7 @@ sub check_msg_49_2
 	{
 		$err_cnt += 5;
 		$err_msg .= "	module: message [n/m]\n\n";
-		$err_msg .= "	PD#XXXX\n\n";
+		$err_msg .= "	PD#SWPL-XXXX\n\n";
 		$err_msg .= "	Problem:\n	detailed description\n\n";
 		$err_msg .= "	Solution:\n	detailed description\n\n";
 		$err_msg .= "	Verify:\n	detailed description\n\n";
@@ -157,10 +157,10 @@ sub check_msg_49_2
 		$err_msg .= "	$err_cnt: Should be no 'kernel' in kernel commit message\n";
 	}
 
-	if( $str[++ $i] !~ /^PD\#.+(\d)$/ )
+	if( $str[++ $i] !~ /^PD\#SWPL-.+(\d)$/ )
 	{
 		$err_cnt += 1;
-		$err_msg .= "	$err_cnt: PD#XXXX\n";
+		$err_msg .= "	$err_cnt: PD#SWPL-XXXX\n";
 
 	}
 
