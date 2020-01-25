8fbb4856f424e9cf584b4334b93608f398597dd1
Arthur Liberman <arthur_liberman@hotmail.com>
2020-01-25 02:09:01 +0200

    osd: don't call osd_wait_vsync_event during HW decoded video playback

--- drivers/amlogic/media/osd/osd_fb.c
+++ drivers/amlogic/media/osd/osd_fb.c
@@ -53,6 +53,9 @@
 #ifdef CONFIG_INSTABOOT
 #include <linux/amlogic/instaboot/instaboot.h>
 #endif
+#ifdef CONFIG_AMLOGIC_VPU
+#include <linux/amlogic/media/vpu/vpu.h>
+#endif
 /* Local Headers */
 #include "osd.h"
 #include "osd_fb.h"
@@ -793,18 +796,27 @@ static int osd_ioctl(struct fb_info *info, unsigned int cmd, unsigned long arg)
 		ret = copy_from_user(&sync_request, argp,
 				sizeof(struct fb_sync_request_s));
 		break;
+	// Only wait for vsync when not HW decoding.
 	case FBIO_WAITFORVSYNC:
-		if (info->node < osd_meson_dev.viu1_osd_count)
-			vsync_timestamp = (s32)osd_wait_vsync_event();
-		else
-			vsync_timestamp = (s32)osd_wait_vsync_event_viu2();
+		if (get_vpu_mem_pd_vmod(VPU_VIU_VD1)) {
+			if (info->node < osd_meson_dev.viu1_osd_count)
+				vsync_timestamp = (s32)osd_wait_vsync_event();
+			else
+				vsync_timestamp = (s32)osd_wait_vsync_event_viu2();
+		} else {
+			vsync_timestamp = 0;
+		}
 		ret = copy_to_user(argp, &vsync_timestamp, sizeof(s32));
 		break;
 	case FBIO_WAITFORVSYNC_64:
-		if (info->node < osd_meson_dev.viu1_osd_count)
-			vsync_timestamp_64 = osd_wait_vsync_event();
-		else
-			vsync_timestamp_64 = osd_wait_vsync_event_viu2();
+		if (get_vpu_mem_pd_vmod(VPU_VIU_VD1)) {
+			if (info->node < osd_meson_dev.viu1_osd_count)
+				vsync_timestamp_64 = osd_wait_vsync_event();
+			else
+				vsync_timestamp_64 = osd_wait_vsync_event_viu2();
+		} else {
+			vsync_timestamp_64 = 0;
+		}
 		ret = copy_to_user(argp, &vsync_timestamp_64, sizeof(s64));
 		break;
 	case FBIOGET_OSD_SCALE_AXIS:
