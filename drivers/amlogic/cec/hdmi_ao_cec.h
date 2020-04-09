b96e4f79cbd05801aedeb64f6118cde836975a82
Yong Qin <yong.qin@amlogic.com>
2020-04-09 20:36:45 +0800

    cec: cec a bus busy cause system hold [1/1]
    PD#SWPL-23547
    
    Problem:
    when spin lock, recursive call will cause system hold.
    
    Solution:
    void recussive calling.
    
    Verify:
    P21AS-S905X
    
    Signed-off-by: Yong Qin <yong.qin@amlogic.com>
    Change-Id: I3616aa198f73a27396ac672b57b729ea30215476

--- drivers/amlogic/cec/hdmi_ao_cec.h
+++ drivers/amlogic/cec/hdmi_ao_cec.h
@@ -18,7 +18,7 @@
 #ifndef __AO_CEC_H__
 #define __AO_CEC_H__
 
-#define CEC_DRIVER_VERSION     "2019/12/09:sm1 bus check reg define err\n"
+#define CEC_DRIVER_VERSION     "2020/04/09:fix cec a recursive call\n"
 
 #define CEC_FRAME_DELAY		msecs_to_jiffies(30)
 #define CEC_CHK_BUS_CNT		20
