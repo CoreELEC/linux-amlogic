83d581feda25a3bcbac790c462eb8eacc56e777f
Davin622 <kodongling@hotmail.com>
2021-04-26 11:06:51 +0800

    add support for tbs5930

---
+++ drivers/media/usb/dvb-usb/tbs5930.h
@@ -0,0 +1,8 @@
+#ifndef _TBS5930_H_
+#define _TBS5930_H_
+
+#define DVB_USB_LOG_PREFIX "tbs5930"
+#include "dvb-usb.h"
+
+#define deb_xfer(args...) dprintk(dvb_usb_tbs5930_debug, 0x02, args)
+#endif
