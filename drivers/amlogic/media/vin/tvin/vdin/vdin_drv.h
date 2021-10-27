1aa286c2ca941c5ff026a83481d7a7d1d03b1e93
qiang.liu <qiang.liu@amlogic.com>
2021-10-27 16:25:51 +0800

    vdin: dts config for t5w bringup [1/1]
    PD#SWPL-62826
    
    Problem:
    t5w bringup
    
    Solution:
    add vdin to t5w
    
    Verify:
    t5w
    
    Change-Id: I798b6cc3db2c76e80286e2bdfd905c159515c4f6
    Signed-off-by: qiang.liu <qiang.liu@amlogic.com>

--- drivers/amlogic/media/vin/tvin/vdin/vdin_drv.h
+++ drivers/amlogic/media/vin/tvin/vdin/vdin_drv.h
@@ -123,12 +123,8 @@ enum vdin_hw_ver_e {
 	 */
 	VDIN_HW_S4,
 	VDIN_HW_S4D,
-	/*
-	 * t3 removed canvas
-	 * no DV meta data slicer
-	 * dv 422 to 444, and to 10 bit 422 (fix shrink issue)
-	 */
 	VDIN_HW_T3,
+	VDIN_HW_T5W,
 };
 
 enum vdin_irq_flg_e {
