d763851df2b938d80cadea975da5830bf2f42128
Evoke Zhang <evoke.zhang@amlogic.com>
2021-12-10 23:05:09 +0800

    backlight: ldim: recover spi dma async rx_buf [1/1]
    PD#SWPL-68259
    
    Problem:
    spi dma transfer abnormal sometime without rx_buf
    
    Solution:
    add rx_buf for spi dma transfer to avoid spi mistake
    
    Verify:
    ar301
    
    Change-Id: I7b8af04461b221cd50fb3d2caf38594be97076b8
    Signed-off-by: Evoke Zhang <evoke.zhang@amlogic.com>

--- drivers/amlogic/media/vout/lcd/backlight/bl_ldim/ldim_spi.c
+++ drivers/amlogic/media/vout/lcd/backlight/bl_ldim/ldim_spi.c
@@ -97,7 +97,7 @@ static void ldim_spi_async_callback(void *arg)
 }
 
 int ldim_spi_write_async(struct spi_device *spi, unsigned char *tbuf,
-			 int tlen, int dma_mode, int max_len)
+			 unsigned char *rbuf, int tlen, int dma_mode, int max_len)
 {
 	unsigned int n, xlen;
 	int ret;
@@ -123,7 +123,7 @@ int ldim_spi_write_async(struct spi_device *spi, unsigned char *tbuf,
 	}
 
 	ldim_spi_async_busy = 1;
-	ret = dirspi_async(spi, tbuf, NULL, xlen,
+	ret = dirspi_async(spi, tbuf, rbuf, xlen,
 		ldim_spi_async_callback, (void *)&ldim_spi_async_busy);
 	if (ret)
 		LDIMERR("%s\n", __func__);
