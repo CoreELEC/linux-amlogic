d8ab1371eeeda0c227bd06513854facc74bed4f2
member3 <jb.dong@bee-link.com>
2022-04-07 17:39:45 +0800

    add phy RTL8211F-VD

--- drivers/net/phy/realtek.c
+++ drivers/net/phy/realtek.c
@@ -937,6 +937,18 @@ static struct phy_driver realtek_drvs[] = {
 		.resume		= rtl821x_resume,
 		.read_page	= rtl821x_read_page,
 		.write_page	= rtl821x_write_page,
+	},{
+		PHY_ID_MATCH_EXACT(0x001cc878),
+		.name		= "RTL8211F-VD Gigabit Ethernet",
+		.probe		= rtl821x_probe,
+		.config_init	= &rtl8211f_config_init,
+		.read_status	= rtlgen_read_status,
+		.config_intr	= &rtl8211f_config_intr,
+		.handle_interrupt = rtl8211f_handle_interrupt,
+		.suspend	= genphy_suspend,
+		.resume		= rtl821x_resume,
+		.read_page	= rtl821x_read_page,
+		.write_page	= rtl821x_write_page,
 	}, {
 		.name		= "Generic FE-GE Realtek PHY",
 		.match_phy_device = rtlgen_match_phy_device,
