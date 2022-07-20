3e8610b9f5fcee42e13bf97c7f6ff74ad41ed42e
Davin622 <kodongling@hotmail.com>
2022-07-20 17:18:30 +0800

    add support for tbs5530/5230

---
+++ drivers/media/usb/dvb-usb/tbs5530.h
@@ -0,0 +1,8 @@
+#ifndef _TBS5530_H_
+#define _TBS5530_H_
+
+#define DVB_USB_LOG_PREFIX "tbs5530"
+#include "dvb-usb.h"
+
+#define deb_xfer(args...) dprintk(dvb_usb_tbs5530_debug, 0x02, args)
+#endif
