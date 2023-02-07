c2de9613b90f5bfe09e28fe2b6b3a616be3e90de
Wanwei Jiang <wanwei.jiang@amlogic.com>
2023-02-07 17:19:36 +0800

    Merge branch 'android14-5.15' into amlogic-5.15-dev [1/1]
    PD#SWPL-87897
    
    Problem:
    record merge upstream information
    
    Solution:
    AML_KERNEL_VERSION      14
    UPSTREAM_VERSION        "5804ade57a27f"
    AML_PATCH_VERSION       "5804ade57a27f"
    MERGE_DATE              "2025-11-03"
    
    Verify:
    sc2
    
    Change-Id: I69f8a7d5d122dec79a63283b9def5eea59a50559
    Signed-off-by: Wanwei Jiang <wanwei.jiang@amlogic.com>

---
+++ include/linux/upstream_version.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
+
+#ifndef _LINUX_UPSTREAM_VERSION_H
+#define _LINUX_UPSTREAM_VERSION_H
+
+#define AML_KERNEL_VERSION	14
+#define UPSTREAM_VERSION	"5804ade57a27f"
+#define AML_PATCH_VERSION	"5804ade57a27f"
+#define MERGE_DATE		"2025-11-03"
+
+#endif
