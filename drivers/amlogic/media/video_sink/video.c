ce28215e8dff62c89f5ea57000dcf4282ec040da
Pengcheng Chen <pengcheng.chen@amlogic.com>
2023-03-23 14:21:07 +0800

    vpp: adjust pip alpha set when pi enable [1/1]
    PD#SWPL-113773
    
    Problem:
    2 channel video blank screen when pi enable
    
    Solution:
    adjust pip alpha set when pi enable
    
    Verify:
    s5
    
    Change-Id: If2878474e21f0089f929848bec182ef0ea971c8b
    Signed-off-by: Pengcheng Chen <pengcheng.chen@amlogic.com>

--- drivers/amlogic/media/video_sink/video.c
+++ drivers/amlogic/media/video_sink/video.c
@@ -5794,9 +5794,6 @@ s32 pip2_render_frame(struct video_layer_s *layer, const struct vinfo_s *vinfo)
 		proc_vd_vsc_phase_per_vsync
 			(layer,
 			frame_par, dispbuf);
-
-		/* update alpha win */
-		alpha_win_set(layer);
 	}
 
 	if (!layer->new_vpp_setting && !force_setting) {
@@ -5841,6 +5838,7 @@ s32 pip2_render_frame(struct video_layer_s *layer, const struct vinfo_s *vinfo)
 		(layer, &layer->sc_setting, vinfo);
 	vd_scaler_setting
 		(layer, &layer->sc_setting);
+	alpha_win_set(layer);
 
 	config_vd_blend
 		(layer, &layer->bld_setting);
@@ -5958,8 +5956,6 @@ s32 pip_render_frame(struct video_layer_s *layer, const struct vinfo_s *vinfo)
 		proc_vd_vsc_phase_per_vsync
 			(layer,
 			frame_par, dispbuf);
-		/* update alpha win */
-		alpha_win_set(layer);
 	}
 
 	if (!layer->new_vpp_setting && !force_setting) {
@@ -6003,6 +5999,7 @@ s32 pip_render_frame(struct video_layer_s *layer, const struct vinfo_s *vinfo)
 	config_vd_pps
 		(layer, &layer->sc_setting, vinfo);
 	vd_s5_hw_set(layer, dispbuf, frame_par);
+	alpha_win_set(layer);
 	vd_scaler_setting
 		(layer, &layer->sc_setting);
 
@@ -6331,8 +6328,6 @@ s32 primary_render_frame(struct video_layer_s *layer)
 
 		/* Do 3D process if enabled */
 		switch_3d_view_per_vsync(layer);
-		/* update alpha win */
-		alpha_win_set(layer);
 	}
 
 	/* no frame parameter change */
@@ -6440,6 +6435,7 @@ s32 primary_render_frame(struct video_layer_s *layer)
 	}
 #endif
 	vd_s5_hw_set(layer, dispbuf, frame_par);
+	alpha_win_set(layer);
 	vd_scaler_setting(layer, &layer->sc_setting);
 	aisr_scaler_setting(layer, &layer->aisr_sc_setting);
 	vd_blend_setting(layer, &layer->bld_setting);
