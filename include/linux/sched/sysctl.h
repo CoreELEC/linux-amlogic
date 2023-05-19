29a055a01a8ca154def5e64102cb7c7167b1de42
qiankun.wang <qiankun.wang@amlogic.com>
2023-05-19 15:26:35 +0800

    sched: vts: vts_kernel_proc_file_api_test [2/2]
    PD#SWPL-124519
    
    Problem:
    vts_kernel_proc_file_api_test module 3 fail
    
    Solution:
    add 3 proc file interfaces 'sched_latency_ns',
    'sched_wakeup_granularity_ns',
    'sched_tunable_scaling'
    
    Verify:
    SC2
    
    Change-Id: I514cea758d73825252a14cb7c39e1f233d075f2e
    Signed-off-by: qiankun.wang <qiankun.wang@amlogic.com>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -14,6 +14,11 @@ extern unsigned long sysctl_hung_task_timeout_secs;
 enum { sysctl_hung_task_timeout_secs = 0 };
 #endif
 
+#if IS_ENABLED(CONFIG_AMLOGIC_BGKI_SCHED_SYSCTL)
+extern unsigned int sysctl_sched_latency;
+extern unsigned int sysctl_sched_min_granularity;
+extern unsigned int sysctl_sched_wakeup_granularity;
+#endif
 extern unsigned int sysctl_sched_child_runs_first;
 
 enum sched_tunable_scaling {
@@ -23,6 +28,13 @@ enum sched_tunable_scaling {
 	SCHED_TUNABLESCALING_END,
 };
 
+#if IS_ENABLED(CONFIG_AMLOGIC_BGKI_SCHED_SYSCTL)
+extern enum sched_tunable_scaling sysctl_sched_tunable_scaling;
+
+int sched_proc_update_handler(struct ctl_table *table, int write,
+		void __user *buffer, size_t *length,
+		loff_t *ppos);
+#endif
 /*
  *  control realtime throttling:
  *
