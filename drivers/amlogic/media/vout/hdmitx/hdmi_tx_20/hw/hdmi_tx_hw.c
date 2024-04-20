35abaaa771e177c95d1b8e418792f4010c094e46
CPM <cpm@example.com>
2024-04-20 01:33:38 +0000

    Add parameter to control clearing of colorimetry flag for tv-led dovi

--- drivers/amlogic/media/vout/hdmitx/hdmi_tx_20/hw/hdmi_tx_hw.c
+++ drivers/amlogic/media/vout/hdmitx/hdmi_tx_20/hw/hdmi_tx_hw.c
@@ -4180,6 +4180,10 @@ static int hdmitx_cntl_config(struct hdmitx_dev *hdev, unsigned int cmd,
 	case CONF_CLR_AUDINFO_PACKET:
 		break;
 	case CONF_AVI_BT2020:
+		if (argv == SET_AVI_NO_CM) {
+			hdmitx_set_reg_bits(HDMITX_DWC_FC_AVICONF1, 0, 6, 2);
+			hdmitx_set_reg_bits(HDMITX_DWC_FC_AVICONF2, 0, 4, 3);
+		}
 		if (argv == SET_AVI_BT2020) {
 			hdmitx_set_reg_bits(HDMITX_DWC_FC_AVICONF1, 3, 6, 2);
 			hdmitx_set_reg_bits(HDMITX_DWC_FC_AVICONF2, 6, 4, 3);
