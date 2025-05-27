e20260d8a3f8e3d569978fe5c8a71aba408261db
jing.li1 <jing.li1@amlogic.com>
2025-05-27 15:45:45 +0800

    dvb: include standard linux dvb header files [1/1]
    PD#SWPL-215766
    
    Problem:
    not support standard linux dvb header files
    
    Solution:
    include standard linux dvb header files
    
    Verify:
    t6w
    
    Change-Id: Iec2f9ed3c170008b7b03c978e1b4a3978e39fe0e
    Signed-off-by: jing.li1 <jing.li1@amlogic.com>

--- include/uapi/linux/dvb/aml_fe_ext.h
+++ include/uapi/linux/dvb/aml_fe_ext.h
@@ -10,11 +10,7 @@
 
 #include <linux/videodev2.h>
 
-#ifdef __KERNEL__
 #include <linux/dvb/frontend.h>
-#else
-#include "frontend.h"
-#endif
 
 #define FE_ANALOG		(FE_ATSC + 1)
 #define FE_DTMB			(FE_ANALOG + 1)
